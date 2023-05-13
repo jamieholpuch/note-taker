e0617919d01fa62774a39e60fbb446dffc580466
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-05-13 07:57:08 -0400

    working on bug fix

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const uuid = require('./helpers/uuid');
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index/')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes/')));
+app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/notes', (req, res) => res.sendFile(__dirname + '/public/notes.html'));
 app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.post('/api/notes', (req, res) => {
