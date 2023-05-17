83c0f7b3bcb5895ec946e11d65a5b3f7c188c016
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-05-16 21:36:41 -0400

    adding heroku files

--- server.js
+++ server.js
@@ -1,64 +1,71 @@
 // Import dependencies
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 const noteData = require('./db/db.json')
 const uuid = require('./helpers/uuid');
+const fs = require('fs');
 
-
+//import middleware
 app.use(express.static('public'));
+app.use(express.json())
 
-app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-app.get('/notes', (req, res) => res.sendFile(__dirname + '/public/notes.html'));
+
+//GET routes
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.get('/api/notes', (req, res) => res.json(noteData));
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
 
-if (title && text) {
-    const { title, text } = req.body;
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
     const newNote = {
-        title,
-        text,
-        note_id: uuid(),
+      title,
+      text,
+      upvotes: Math.floor(Math.random() * 100),
+      review_id: uuid(),
     };
 
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-    // Convert string into JSON object
-    const parsedNotes = JSON.parse(data);
-  
-    // Add a new note
-    parsedNotes.push(newNote);
-  
-    // Write updated notes back to the file
-    fs.writeFile(
-        './db/db.json',
-        JSON.stringify(parsedNotes, null, 2),
-        (writeErr) =>
-            writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-          );
-        }
-    });
+    // Convert the data to a string so we can save it
+    const noteString = JSON.stringify(newNote);
+
+    // Write the string to a file
+    fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newNote.title} has been written to JSON file`
+          )
+    );
 
     const response = {
-        status: 'success',
-        body: newNote,
+      status: 'success',
+      body: newNote,
     };
 
     console.log(response);
     res.status(201).json(response);
-} else {
-    res.status(500).json('Error in posting note')
-};
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
 );
-});
